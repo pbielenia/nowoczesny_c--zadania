5812c3bc6bea549023e08e2499f28b982c38b06b
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-06-28 17:18:00 +0200

    Update 16-enumerating IPv4 address

---
+++ scripts/run-tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo cmake --build build --target test -- ARGS=\"-R \"$1:\"\"
---
+++ src/016-enumerating_ipv4_address_in_a_range/include/enumerating_ipv4_address_in_a_range/IPv4Address.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class IPv4Address {
+public:
+    static const unsigned short length{4};
+
+    IPv4Address(const std::string& address);
+    ~IPv4Address() = default;
+
+    bool operator==(const IPv4Address& address) const;
+    bool operator<(const IPv4Address& address) const;
+    bool operator>(const IPv4Address& address) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const IPv4Address& obj);
+    friend std::istream& operator>>(std::istream& is, IPv4Address& obj);
+
+    void from_str(const std::string& address);
+    std::string to_str() const;
+
+private:
+    static const char m_delimiter = '.';
+
+    std::vector<std::uint8_t> m_address;
+};
+
+std::vector<std::string> split(const std::string& s, char delimiter);
